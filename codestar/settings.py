2f2b3078790b1a222ea1d7b4116d43fb41516dae
Daniel Mullooly <mulloolyd.1998@gmail.com>
2024-05-24 18:35:27 +0000

    Fix migrate blog url issue

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com","8000-mulloo-djangoblog-x73v7z98y2l.ws-eu110.gitpod.io"]
 
 
